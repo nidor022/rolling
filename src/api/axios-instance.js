40f9b95dac00b5392e5dd4449c8afa3559a2772a
luli <nidor022@gmail.com>
2026-01-29 00:11:34 +0900

    누락된파일 추가및 안쓰이는 파일 삭제

--- src/api/axios-instance.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://rolling-api.vercel.app/",
-  timeout: 5000,
-  headers: { "Content-Type": "application/json" },
-});
-
-export default axiosInstance;
